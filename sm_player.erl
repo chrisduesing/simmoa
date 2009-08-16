571326163e8fb3385c915d7c18ea2dc7b4a18671
Chris Duesing <cwac5@hotmail.com>
2009-08-16 18:00:58 -0500

    sm_player now supports multiple users

--- sm_player.erl
+++ sm_player.erl
@@ -3,14 +3,14 @@
  
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
  
--export([move/1, start_link/0]).
+-export([move/2, start_link/1]).
  
-start_link() -> gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+start_link(Player) -> gen_server:start_link({local, Player}, ?MODULE, [Player], []).
 
-move(Direction) -> gen_server:call(?MODULE, {move, Direction}).
+move(Player, Direction) -> gen_server:call(Player, {move, Direction}).
 
-init([]) ->
-       Tab = ets:new(?MODULE, []),
+init([Player]) ->
+       Tab = ets:new(Player, []),
        {ok, Tab}.
  
 handle_call({move, Direction}, _From, Tab) ->
