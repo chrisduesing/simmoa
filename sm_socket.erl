5e36a655c2601380e8cce319fcf9a5c55642c801
Chris Duesing <cwac5@hotmail.com>
2009-08-17 22:09:47 -0500

    socket now logs player in and takes commands.

--- sm_socket.erl
+++ sm_socket.erl
@@ -3,16 +3,30 @@
 -export([test/0]).
 
 test() ->
-       {ok, ListenSocket} = gen_tcp:listen(5555,[{active,false}]),
+       {ok, ListenSocket} = gen_tcp:listen(5558,[{active,false}]),
        accept(ListenSocket).
 
 accept(ListenSocket) ->
 	{ok, Socket} = gen_tcp:accept(ListenSocket),
-	spawn(fun() -> handle(Socket) end),
+	Player = login(Socket),
+	spawn(fun() -> handle(Player, Socket) end),
 	accept(ListenSocket).
 
-handle(Socket) ->
-	       {ok, Data} = gen_tcp:recv(Socket, 0),
-	       gen_tcp:send(Socket, Data),
-	       handle(Socket).
+login(Socket) ->
+	      gen_tcp:send(Socket, "username>"),
+	      {ok, Data} = gen_tcp:recv(Socket, 0),
+	      Player = list_to_atom(Data),
+	      sm_player:start_link(Player),
+	      Player.
+
+
+handle(Player, Socket) ->
+	       {ok, RawData} = gen_tcp:recv(Socket, 0),
+	       Data = string:strip(
+			 string:strip(RawData, right, $\n),
+			 right, $\r),
+	       Command = list_to_atom(Data),
+	       Response = sm_player:move(Player, Command),
+	       gen_tcp:send(Socket, term_to_binary(Response)),
+	       handle(Player, Socket).
 		
