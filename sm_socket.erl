0c53396145991a45f0a027fcac43912c9b5ed64d
Chris Duesing <cwac5@hotmail.com>
2009-08-19 22:31:20 -0500

    client crash now isolated to their process, will not stop accepting connections for other players. need to otp-ify it now.

--- sm_socket.erl
+++ sm_socket.erl
@@ -2,32 +2,42 @@
 
 -export([test/0]).
 
+
 test() ->
        {ok, ListenSocket} = gen_tcp:listen(5550,[{active,false}]),
        accept(ListenSocket).
 
+
 accept(ListenSocket) ->
 	{ok, Socket} = gen_tcp:accept(ListenSocket),
-	Player = login(Socket),
-	spawn(fun() -> handle(Player, Socket) end),
+	spawn(fun() -> catch handle(Socket) end),
 	accept(ListenSocket).
 
+
+handle(Socket) ->
+	  try
+	       Player = login(Socket),
+	       loop(Player, Socket)
+	  catch
+		error:_Reason ->
+		    gen_tcp:close(Socket)
+	  end.
+
 login(Socket) ->
-	      gen_tcp:send(Socket, "username>"),
+	      gen_tcp:send(Socket, "username> "),
 	      {ok, Data} = gen_tcp:recv(Socket, 0),
 	      Player = list_to_atom(Data),
 	      sm_player:start_link(Player),
 	      Player.
 
 
-handle(Player, Socket) ->
-	       {ok, RawData} = gen_tcp:recv(Socket, 0),
-	       Data = string:strip(
-			 string:strip(RawData, right, $\n),
-			 right, $\r),
-	       Command = list_to_atom(Data),
-	       {X, Y} = sm_player:move(Player, Command),
-	       Response = ["You moved to ", X, ", ", Y],
-	       gen_tcp:send(Socket, Response),
-	       handle(Player, Socket).
-		
+loop(Player, Socket) ->
+	       	gen_tcp:send(Socket, "> "),
+	        {ok, RawData} = gen_tcp:recv(Socket, 0),
+	       	Data = string:strip(string:strip(RawData, right, $\n), right, $\r),
+		Command = list_to_atom(Data),
+	       	{X, Y} = sm_player:move(Player, Command),
+	       	Response = ["You moved to ", X, ", ", Y, "\r\n"],
+	       	gen_tcp:send(Socket, Response),
+	       	loop(Player, Socket).
+		    
